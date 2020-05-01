54a77a2be28773a14842771d42a17d2d375132b9
vitormalencar <vitor.maia.alencar@gmail.com>
2020-05-01 14:06:03 +0200

    update check as middleware

--- src/verifySignature.js
+++ src/verifySignature.js
@@ -1,20 +1,21 @@
-const crypto = require('crypto');
-const timingSafeCompare = require('tsscmp');
+import crypto from 'crypto'
+import timingSafeCompare from 'tsscmp'
 
-const isVerified = (req) => {
-  const signature = req.headers['x-slack-signature'];
-  const timestamp = req.headers['x-slack-request-timestamp'];
-  const hmac = crypto.createHmac('sha256', process.env.SLACK_SIGNING_SECRET);
-  const [version, hash] = signature.split('=');
+export const isVerified = (req, res, next) => {
+  let validUser = false
+  const signature = req.headers['x-slack-signature']
+  const timestamp = req.headers['x-slack-request-timestamp']
+  const hmac = crypto.createHmac('sha256', process.env.SLACK_SIGNING_SECRET)
 
-  // Check if the timestamp is too old
-  const fiveMinutesAgo = ~~(Date.now() / 1000) - (60 * 5);
-  if (timestamp < fiveMinutesAgo) return false;
+  const fiveMinutesAgo = ~~(Date.now() / 1000) - 60 * 5
 
-  hmac.update(`${version}:${timestamp}:${req.rawBody}`);
+  const isOld = timestamp < fiveMinutesAgo
 
-  // check that the request signature matches expected value
-  return timingSafeCompare(hmac.digest('hex'), hash);
-};
-
-module.exports = { isVerified };
+  if (signature !== undefined && !isOld) {
+    const [version, hash] = signature.split('=')
+    // Check if the timestamp is too old
+    hmac.update(`${version}:${timestamp}:${req.rawBody}`)
+    validUser = timingSafeCompare(hmac.digest('hex'), hash)
+  }
+  return validUser ? next() : res.status(404).send('Not valid request')
+}
